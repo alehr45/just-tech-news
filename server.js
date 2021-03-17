9aeb3012fd00e3ed7f9bf241abfae408f31ed7be
Your NameAlex <alehr45@gmail.com>
2021-03-17 15:08:35 -0500

    sequelize sync

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+sequelize.sync({ force:true }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
 
 const sess = {
   secret: 'Super secret secret',
